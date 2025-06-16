db734bd14c3a47cd8e6b039114da2c08895d099a
tykio0415 <tykio0415@gmail.com>
2025-06-16 10:05:29 +0800

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const response = await axios.get(targetUrl, {
   headers: {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/119.0.0.0 Safari/537.36",
     "Accept-Language": "en-US,en;q=0.9",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    "Referer": "https://www.dcard.tw/"
   }
 });
     const $ = cheerio.load(response.data)
